b8defe56e4b2a1acdc334ccde9e4456f9e73b522
DarthNecro95 <149696939+DarthNecro95@users.noreply.github.com>
2023-12-12 00:37:25 +0000

    removed projects.html path

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ app = Flask(__name__)
 def hello_world():
     return render_template('home.html')
 
-@app.route("/templates/")
-def hello_world():
-    return render_template('projects.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
