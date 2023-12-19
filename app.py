6000c9a31ddc4e352882c4d395c2f5a4733e5ffa
DarthNecro95 <149696939+DarthNecro95@users.noreply.github.com>
2023-12-19 23:23:52 +0000

    added route in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, send_from_directory
 
 
 app = Flask(__name__)
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def hello_world():
     return render_template('home.html')
 
+
+@app.route('/reports/<path:path>')
+def send_report(path):
+    return send_from_directory('reports', path)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
